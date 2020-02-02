60056eca937a91bc8312b6503035e7b26995af3a
Phoder1 <Phoder1@gmail.com>
2020-02-03 01:45:18 +0200

    1st Commit

---
+++ Assets/Scripts/Camera2DFollow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityStandardAssets._2D
+{
+    public class Camera2DFollow : MonoBehaviour
+    {
+         GameObject player;
+
+        // Use this for initialization
+        private void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+
+        // Update is called once per frame
+        private void Update()
+        {
+            gameObject.transform.position = new Vector3(player.transform.position.x, player.transform.position.y,gameObject.transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/OpenFurniture.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenFurniture : MonoBehaviour
+{
+    [SerializeField] GameObject openedItem = null;
+
+
+    private void OnMouseOver() {
+        Debug.Log("Mouse is over");
+        if (Input.GetMouseButtonUp(0)) {
+            Debug.Log("Mouse is up");
+            openedItem.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+
+
+
+}
